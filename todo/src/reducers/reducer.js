afa0afd352c398b31682f114c94159b5e3b079bd
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-13 13:19:02 -0600

    finished step 1

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,9 @@
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987489
+}
+
+export const todoReducer = (state, action) => {
+    return state
+}
