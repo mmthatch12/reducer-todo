7f7ba7a23ae3c523a20e5856ce43a403b531241b
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-13 16:22:12 -0600

    finished step 3

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -6,20 +6,36 @@ import { initialState, todoReducer } from '../reducers/reducer'
 import Todo from './Todo'
 
 const TodoForm = () => {
+    const[newTodo, setNewTodo] = useState()
 
     const[state, dispatch] = useReducer(todoReducer, initialState)
     console.log(state)
 
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
+
+    const handleChanges = event => {
+        setNewTodo(event.target.value)
+    }
+
     return(
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input 
                     placeholder='next Todo'
+                    type='text'
                     name='item'
+                    value={newTodo}
+                    onChange={handleChanges}
                 />
-                <button type="submit">Add Todo</button>
+                <button 
+                    onClick={() => 
+                    dispatch({ type: 'ADD_TODO', payload: newTodo })}>Add Todo</button>
             </form>
-            <Todo />
+            {state.things.map(todo => 
+               <h2>{todo.item}</h2>
+            )}
         </div>
 
     )
--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,17 +1,24 @@
 export const initialState = {
+    things:[{
     item: 'Learn about reducers',
     completed: false,
     id: 3892987489
-}
+}]}
 
 export const todoReducer = (state, action) => {
     switch (action.type) {
         case 'ADD_TODO':
             return {
-                ...state,
-                item: action.payload,
+                ...state, 
+                things:[...state.things,
+                {item: action.payload,
                 completed: false,
-                id: new Date()
+                id: Date.now()}]
+            }
+        case 'TOGGLE_COMPLETED':
+            return {
+                ...state,
+                things:state.things.map(thing => {})
             }
         default:
             return state
